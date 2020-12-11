7e8f3871187ebfdebfe245f2c8dceb9d7a29cfd2
saglamorhan61 <saglamorhan.61@gmail.com>
2020-12-11 18:46:33 +0300

    gorsel secme islevi eklendi

--- app/src/main/java/com/saglamorhan/kotlinfrebaseinsta/UploadActivity.kt
+++ app/src/main/java/com/saglamorhan/kotlinfrebaseinsta/UploadActivity.kt
@@ -3,14 +3,21 @@ package com.saglamorhan.kotlinfrebaseinsta
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_upload.*
+import java.lang.Exception
 
 class UploadActivity : AppCompatActivity() {
+
+    var selectedPicture : Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
@@ -29,6 +36,50 @@ class UploadActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        if (requestCode == 1){
+            if (grantResults.size >0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent,2)
+
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null){
+
+            selectedPicture = data.data
+
+            try {
+
+                if (selectedPicture != null){
+
+                    if (Build.VERSION.SDK_INT >=28){
+                        val source = ImageDecoder.createSource(contentResolver,selectedPicture!!)
+                        val bitmap = ImageDecoder.decodeBitmap(source)
+                        uploadImageView.setImageBitmap(bitmap)
+
+                    }else{
+                        val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver,selectedPicture)
+                        uploadImageView.setImageBitmap(bitmap)
+                    }
+
+                }
+
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
     fun uoploadClicked(view: View){
 
     }
