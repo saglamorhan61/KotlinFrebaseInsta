22d26bacbec46f1a1949a3109f9d8e4fae3909d7
saglamorhan61 <saglamorhan.61@gmail.com>
2020-12-11 18:27:49 +0300

    Upload activity eklendi Manifeste izin eklendi.

--- app/src/main/java/com/saglamorhan/kotlinfrebaseinsta/UploadActivity.kt
+++ app/src/main/java/com/saglamorhan/kotlinfrebaseinsta/UploadActivity.kt
@@ -1,11 +1,35 @@
 package com.saglamorhan.kotlinfrebaseinsta
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+    }
+    fun imageViewClicked(view: View){
+
+     //galeriye gitme islemleri
+        //izin islemleri icin
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            //Manifeste izni eklemeyi unutma!!!
+            //izin verilmediyse calisacak
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+        }else{
+            val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent,2)
+        }
+
+    }
+    fun uoploadClicked(view: View){
+
     }
 }
