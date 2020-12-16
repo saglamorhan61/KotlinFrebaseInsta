2cc9c63b37e510ee897a4a66ad8304ad771a5a73
saglamorhan61 <saglamorhan.61@gmail.com>
2020-12-16 17:41:20 +0300

    Veriler Firestore dan alindi.

--- app/src/main/java/com/saglamorhan/kotlinfrebaseinsta/FeedActivity.kt
+++ app/src/main/java/com/saglamorhan/kotlinfrebaseinsta/FeedActivity.kt
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
+    private lateinit var db : FirebaseFirestore
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
@@ -40,5 +44,40 @@ class FeedActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+        getDataFromFirestore()
+    }
+
+
+    fun getDataFromFirestore(){
+
+        db.collection("Posts").addSnapshotListener { snapshot, exception ->
+
+            if (exception != null){
+                Toast.makeText(applicationContext,exception.localizedMessage.toString(),Toast.LENGTH_LONG).show()
+            }else{
+
+                if (snapshot != null){
+                    if (!snapshot.isEmpty){
+
+                        val documents = snapshot.documents
+                        for (document in documents){
+                            val comment = document.get("comment") as String
+                            val useremail = document.get("userEmail") as String
+                            val downoladUrl = document.get("downloadUrl") as String
+                            val timestamp = document.get("date") as Timestamp
+                            val date = timestamp.toDate()
+                            println(comment)
+                            println(useremail)
+                            println(downoladUrl)
+                            println(date)
+                        }
+                    }
+                }
+
+            }
+        }
+
+
     }
 }
